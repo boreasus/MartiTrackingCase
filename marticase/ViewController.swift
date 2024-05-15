2cf06c3aad03a57f3ebb6bff0c7e279a08b925fb
poyraz <cccusccc@gmail.com>
2024-05-15 13:37:29 +0300

    Initial Commit

---
+++ marticase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marticase
+//
+//  Created by safa uslu on 15.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
